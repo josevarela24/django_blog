0af58ca4f24137e1cca1f009d8382a58739a06a8
booble <jose.varela24@yahoo.com>
2019-12-26 00:19:58 -0600

    added crispy forms and updated requirements.txt

--- django_project/settings.py
+++ django_project/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     "blog.apps.BlogConfig",
     "users.apps.UsersConfig",
+    "crispy_forms",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -114,3 +115,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "/static/"
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
