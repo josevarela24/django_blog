73e8482f13727e88f4cc16b00c89cdbec75625aa
booble <jose.varela24@yahoo.com>
2019-12-30 21:57:44 -0600

    changed homme page to list based view

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Post
 
 # Create your views here.
 def home(request):
     context = {"posts": Post.objects.all()}
     return render(request, "blog/home.html", context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/home.html"
+    context_object_name = "posts"
+    ordering = ["-date_posted"]
 
 
 def about(request):
