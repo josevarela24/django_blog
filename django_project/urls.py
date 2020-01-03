dc708b009d6b65ac5b39fef0c3fdc3ce9e92a334
Jose Varela <jose.varela24@yahoo.com>
2020-01-02 20:06:00 -0600

    added reset confirm and smtp gmail auth

--- django_project/urls.py
+++ django_project/urls.py
@@ -40,11 +40,18 @@ urlpatterns = [
         name="password_reset",
     ),
     path(
-        "password_reset/done/",
+        "password-reset/done/",
         auth_views.PasswordResetDoneView.as_view(
             template_name="users/password_reset_done.html"
         ),
         name="password_reset_done",
+    ),
+    path(
+        "password-reset-confirm/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="users/password_reset_confirm.html"
+        ),
+        name="password_reset_confirm",
     ),
     path("", include("blog.urls")),
 ]
